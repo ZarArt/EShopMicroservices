70469b833230b6f418d0adc38cf58a0e59e4e50e
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-11 20:42:29 +0200

    Add common validation and error handler;

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponce>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponce>
+    where TRequest : ICommand<TResponce>
+{
+    public async Task<TResponce> Handle(TRequest request, RequestHandlerDelegate<TResponce> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults.Where(r => r.Errors.Any()).SelectMany(r => r.Errors).ToList();
+
+        if (failures.Any())
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,9 +1,25 @@
-﻿namespace Catalog.API.Products.UpdateProduct;
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductCommand(Guid Id, string Name, List<string> Categories,
     string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
 
 public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater then 0");
+    }
+}
 
 internal class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProductHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
@@ -16,7 +32,7 @@ internal class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProd
 
         if (product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(command.Id);
         }
 
         product.Name = command.Name;
