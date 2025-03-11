429f25267493505064eb665853cbb1374e0ae5c9
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-11 14:11:36 +0200

    add GetProductsByCategory method;

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,5 +1,29 @@
-﻿namespace Catalog.API.Products.GetProductById;
+﻿
+using Catalog.API.Products.GetProducts;
 
-public class GetProductByIdEndpoint
+namespace Catalog.API.Products.GetProductById;
+
+//public record GetProductByIdRequest;
+
+public record GetProductByIdResponce(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}",
+            async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var responce = result.Adapt<GetProductByIdResponce>();
+
+                return Results.Ok(responce);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponce>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product by Id")
+            .WithDescription("Get Product by Id");
+    }
 }
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductsByCategory;
+
+//public record GetProductsByCategoryRequest();
+
+public record GetProductsByCategoryResponce(IEnumerable<Product> Products);
+
+public class GetProductsByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}",
+            async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsByCategoryQuery(category));
+
+                var responce = result.Adapt<GetProductsByCategoryResponce>();
+
+                return Results.Ok(responce);
+            })
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductsByCategoryResponce>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products by Category")
+            .WithDescription("Get Products by Category");
+    }
+}
