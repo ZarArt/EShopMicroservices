eec2182bcf8d2ab51fb15931700e898f0b283813
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-11 18:28:58 +0200

    Add update and delete methods;

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+//public record GetProductByIdRequest;
+
+public record DeleteProductResponce(bool IsSuccess);
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}",
+            async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var responce = result.Adapt<DeleteProductResponce>();
+
+                return Results.Ok(responce);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponce>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+    }
+}
