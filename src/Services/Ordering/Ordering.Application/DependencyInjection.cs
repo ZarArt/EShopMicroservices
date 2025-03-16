e39ad442f396a460b83b838fb226156208574dda
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-16 18:04:43 +0200

    Add Ordering projects to solution;

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static  class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        return serviceCollection;
+    }
+}
