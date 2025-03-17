11d5546d343b563e2d626dc265f39392a7cf63cc
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-17 18:28:12 +0200

    Add domain models to Ordering.API project;

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -2,12 +2,33 @@
 
 public record Address
 {
-    public string Firstname { get; } = default!;
-    public string Lastname { get; } = default!;
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
     public string? EmailAddress { get; } = default!;
     public string AddressLine { get; } = default!;
     public string Country { get; } = default!;
     public string State { get; } = default!;
     public string ZipCode { get; } = default!;
 
+    protected Address()
+    { }
+
+    private Address(string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string zipCode)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        AddressLine = addressLine;
+        Country = country;
+        State = state;
+        ZipCode = zipCode;
+    }
+
+    public static Address Of(string firstName, string lastname, string emailAddress, string addressLine, string country, string state, string zipCode)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+        ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+        return new Address(firstName, lastname, emailAddress, addressLine, country, state, zipCode);
+    }
 }
