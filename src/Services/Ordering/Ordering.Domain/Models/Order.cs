003b3dafdf9cba59020ff1c708c11f7a8feae2ec
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-17 23:36:59 +0200

    Add auto-migration to Ordering.API db;

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -15,6 +15,7 @@ public class Order : Aggregate<OrderId>
     public decimal TotalPrice
     {
         get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
     }
 
     public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
