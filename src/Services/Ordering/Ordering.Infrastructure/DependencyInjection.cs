ef2101e1b6d096247fc5b086b1c7abf4a566575c
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-18 00:28:10 +0200

    Add interceptors for domain events;

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -9,9 +11,12 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+        serviceCollection.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        serviceCollection.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptors>();
+
+        serviceCollection.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             options.UseSqlServer(connectionString);
         });
 
