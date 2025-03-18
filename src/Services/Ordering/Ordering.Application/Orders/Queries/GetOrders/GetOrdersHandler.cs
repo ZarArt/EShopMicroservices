8710c6a51a163fce0338fac0a8ad7fb486372a51
Artem Zaruba <artem.zaruba@globallogic.com>
2025-03-18 17:31:26 +0200

    Add handlers for each feature in Ordering.Application project;

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext applicationDbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationQuery.PageIndex;
+        var pageSize = query.PaginationQuery.PageSize;
+
+        var totalCount = await applicationDbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await applicationDbContext.Orders
+            .Include(o => o.OrderItems)
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(
+            new PaginationResult<OrderDto>(
+                pageIndex,
+                pageSize,
+                totalCount,
+                orders.ToOrderDtoList()));
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,5 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationQuery) : IQuery<GetOrdersResult>;
+
+public record GetOrdersResult(PaginationResult<OrderDto> Orders);
